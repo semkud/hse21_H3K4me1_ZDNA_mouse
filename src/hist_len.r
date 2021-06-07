8284a4a60da4f262b12709f555ab4c4336dba0d3
semkud <semeon1998@gmail.com>
2021-06-07 21:12:39 +0300

    Histogram_of_lengths

---
+++ src/hist_len.r
@@ -0,0 +1,21 @@
+setwd("/")
+source('lib.R')
+
+#NAME <- 'H3K4me1_ENCFF596ORE.mm10'
+NAME <- 'H3K4me1_ENCFF791SEO.mm10'
+
+OUT_DIR <- 'results/'
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+hist(bed_df$len)
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
---
+++ src/lib.r
@@ -0,0 +1,9 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)   # replace_na
+library(tibble)  # column_to_rownames
+
+###
+
+DATA_DIR <- '../data/'
+OUT_DIR <- '../Results/'
