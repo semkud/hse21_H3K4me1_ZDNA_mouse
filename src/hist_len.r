84426e122e0b7ca2596c94b41d8c8bd5769af764
semkud <semeon1998@gmail.com>
2021-06-08 15:02:07 +0300

    histograms

--- src/hist_len.r
+++ src/hist_len.r
@@ -1,11 +1,8 @@
-setwd("/")
+setwd("C:/Users/SKudr/Documents/GitHub/hse21_H3K4me1_ZDNA_mouse/src")
 source('lib.R')
 
-#NAME <- 'H3K4me1_ENCFF596ORE.mm10'
-NAME <- 'H3K4me1_ENCFF791SEO.mm10'
-
-OUT_DIR <- 'results/'
-
+NAME <- 'H3K4me1_ENCFF596ORE.mm10'
+#NAME <- 'H3K4me1_ENCFF791SEO.mm10'
 bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
 colnames(bed_df) <- c('chrom', 'start', 'end', 'name', 'score')
 bed_df$len <- bed_df$end - bed_df$start
