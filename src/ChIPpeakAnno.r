66fb70e55ad473fa45215686902c49d3d701e4f0
semkud <semeon1998@gmail.com>
2021-06-09 00:51:59 +0300

    updated script chippeakanno

--- src/ChIPpeakAnno.r
+++ src/ChIPpeakAnno.r
@@ -9,18 +9,18 @@ BiocManager::install("org.Hs.eg.db")
 BiocManager::install("org.Mm.eg.db")
 
 library(ChIPpeakAnno)
-library(TxDb.Hsapiens.UCSC.hg19.knownGene)
-library(org.Hs.eg.db)
-#library(TxDb.Mmusculus.UCSC.mm10.knownGene)
-#library(org.Mm.eg.db)
+#library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+#library(org.Hs.eg.db)
+library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+library(org.Mm.eg.db)
 
 ###
 
 
-peaks <- toGRanges(paste0(DATA_DIR, 'H3K4me3_A549.intersect_with_DeepZ.bed'), format="BED")
+peaks <- toGRanges(paste0(DATA_DIR, 'H3K4me1_ZDNA.intersect_with_DEEPZ.bed'), format="BED")
 peaks[1:2]
 
-annoData <- toGRanges(TxDb.Hsapiens.UCSC.hg19.knownGene)
+annoData <- toGRanges(TxDb.Mmusculus.UCSC.mm10.knownGene)
 annoData[1:2]
 
 
@@ -30,13 +30,13 @@ anno <- annotatePeakInBatch(peaks, AnnotationData=annoData,
                             bindingRegion=c(-2000, 300))
 data.frame(anno) %>% head()
 
-anno$symbol <- xget(anno$feature, org.Hs.egSYMBOL)
+anno$symbol <- xget(anno$feature, org.Mm.egSYMBOL)
 data.frame(anno) %>% head()
 
 anno_df <- data.frame(anno)
-write.table(anno_df, file=paste0(DATA_DIR, 'H3K4me3_A549.intersect_with_DeepZ.genes.txt'),
+write.table(anno_df, file=paste0(DATA_DIR, 'H3K4me1_ZDNA.intersect_with_DEEPZ.genes.txt'),
             col.names = TRUE, row.names = FALSE, sep = '\t', quote = FALSE)
 
 uniq_genes_df <- unique(anno_df['symbol'])
-write.table(uniq_genes_df, file=paste0(DATA_DIR, 'H3K4me3_A549.intersect_with_DeepZ.genes_uniq.txt'),
+write.table(uniq_genes_df, file=paste0(DATA_DIR, 'H3K4me1_ZDNA.intersect_with_DEEPZ.genes_uniq.txt'),
             col.names = FALSE, row.names = FALSE, sep = '\t', quote = FALSE)
